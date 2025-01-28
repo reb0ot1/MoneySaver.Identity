9ba5b296285cb83496a355bb4ad9be882a5bafff
Svet <sbojinov@yahoo.com>
2025-01-28 20:03:22 +0200

    Upgrade to .NET9

--- MoneySaver.Identity/Services/Identity/TokenGeneratorService.cs
+++ MoneySaver.Identity/Services/Identity/TokenGeneratorService.cs
@@ -2,6 +2,7 @@
 using Microsoft.IdentityModel.Tokens;
 using MoneySaver.Identity.Data.Models;
 using MoneySaver.System;
+using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -21,8 +22,8 @@ namespace MoneySaver.Identity.Services.Identity
             var key = Encoding.ASCII.GetBytes(this.applicationSettings.Secret);
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.NameIdentifier, user.Id),
-                new Claim(ClaimTypes.Name, user.Email)
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
             };
 
             if (roles != null)
@@ -34,7 +35,9 @@ namespace MoneySaver.Identity.Services.Identity
             {
                 Subject = new ClaimsIdentity(claims),
                 //TODO: Add the expire parameter in appsettings
-                Expires = DateTime.UtcNow.AddDays(7),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                //Issuer = "issuer of the token",
+                //Audience = "where it will be used"
                 SigningCredentials = new SigningCredentials(
                     new SymmetricSecurityKey(key),
                     SecurityAlgorithms.HmacSha256Signature)
